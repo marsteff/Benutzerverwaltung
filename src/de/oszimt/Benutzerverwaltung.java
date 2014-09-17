8d6a786b511af13ce458a607e0627965858d542b
Marcus Münzberg <marsteff@gmail.com>
2014-09-17 14:22:45 +0200

    start working on tui

--- src/de/oszimt/Benutzerverwaltung.java
+++ src/de/oszimt/Benutzerverwaltung.java
@@ -4,13 +4,14 @@ import de.oszimt.conzept.impl.Concept;
 import de.oszimt.factory.PersistanceFactory;
 import de.oszimt.persistence.enumeration.PersistanceMethod;
 import de.oszimt.ui.Gui;
+import de.oszimt.ui.Tui;
 
 /**
  * Created by Philipp on 17.09.14.
  */
 public class Benutzerverwaltung {
     public static void main(String[] args) {
-        new Gui(new Concept(PersistanceFactory.buildPersistance(PersistanceMethod.SQLITE)));
-        //System.out.println("xs");
+//        new Gui(new Concept(PersistanceFactory.buildPersistance(PersistanceMethod.SQLITE)));
+        new Tui();
     }
 }
--- src/de/oszimt/ui/Tui.java
+++ src/de/oszimt/ui/Tui.java
@@ -1,7 +1,41 @@
 package de.oszimt.ui;
 
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
+
 /**
  * Created by user on 17.09.2014.
  */
 public class Tui {
+
+    public Tui(){
+        AnsiConsole.systemInstall();
+        println(GREEN, "dududu");
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        clean();
+        println(BLUE, "Hallo");
+    }
+
+    public void showMainMenu() {
+
+    }
+
+    private void println(Color color, String text){
+        System.out.println(ansi().fg(color).a(text).fg(BLACK));
+    }
+
+    private void print(Color color, String text){
+        System.out.print(ansi().fg(color).a(text).fg(BLACK));
+    }
+
+    private void clean(){
+        System.out.println(ansi().eraseScreen());
+    }
+
 }
