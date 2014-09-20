575de83fa6cfd61fcabca1f17cb333d86b766f4d
marsteff <marsteff@gmail.com>
2014-09-20 13:42:29 +0200

    restore everthing ... have done stupid shit

--- src/de/oszimt/Benutzerverwaltung.java
+++ src/de/oszimt/Benutzerverwaltung.java
@@ -4,6 +4,7 @@ import de.oszimt.concept.enumeration.ConceptMethod;
 import de.oszimt.factory.ConceptFactory;
 import de.oszimt.factory.PersistanceFactory;
 import de.oszimt.persistence.enumeration.PersistanceMethod;
+import de.oszimt.ui.Gui;
 import de.oszimt.ui.Tui;
 
 /**
@@ -11,7 +12,7 @@ import de.oszimt.ui.Tui;
  */
 public class Benutzerverwaltung {
     public static void main(String[] args) {
-//        new Gui(new Concept(PersistanceFactory.buildPersistance(PersistanceMethod.SQLITE)));
-        new Tui(ConceptFactory.buildConcept(ConceptMethod.STANDARD_CONCEPT, PersistanceFactory.buildPersistance(PersistanceMethod.SQLITE)));
+        new Gui(ConceptFactory.buildConcept(ConceptMethod.STANDARD_CONCEPT, PersistanceFactory.buildPersistance(PersistanceMethod.SQLITE)));
+//        new Tui(ConceptFactory.buildConcept(ConceptMethod.STANDARD_CONCEPT, PersistanceFactory.buildPersistance(PersistanceMethod.SQLITE)));
     }
 }
